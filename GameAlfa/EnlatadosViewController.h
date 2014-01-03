6ca455b67cc62e898b1323f39e02e0c0ec63a14a
alvanxp <alvanxp@outlook.com>
2014-01-02 23:56:55 -0500

    Added Enlatado View Controller

---
+++ GameAlfa/EnlatadosViewController.h
@@ -0,0 +1,13 @@
+//
+//  EnlatadosViewController.h
+//  GameAlfa
+//
+//  Created by lion carp on 1/2/14.
+//  Copyright (c) 2014 Alvan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnlatadosViewController : UITableViewController
+
+@end
